7ca83ea4eb4bb2259513f3a00c0a59f4337998f8
Jan Stiborek <jan.stiborek@agents.fel.cvut.cz>
2012-10-10 08:33:45 +0200

    lcdExample changed - it uses Fleury's LCD library

--- examples/lcdExample/main.c
+++ examples/lcdExample/main.c
@@ -1,28 +1,29 @@
 #include <avr/io.h>
+#include <util/delay.h>
+
 #include "lcd.h"
 
-int main() {
-	DDRA = 0xff;
-	DDRC = 0xff;
+int main(void) {
+	lcd_init(LCD_DISP_ON); /* initialize display, cursor off */
 
-	_delay_ms(500);
-	LCD_init();
 
-	LCD('A');
-	LCD('H');
-	LCD('O');
-	LCD('J');
-
-	while(1){
+	while (1) /* loop forever */
+	{
+		lcd_home();
+		lcd_clrscr(); /* clear the screen*/
+		lcd_puts("test program 1"); /* displays the string on lcd*/
+		lcd_gotoxy(0,1);
+		_delay_ms(1000);
+		lcd_puts("test program 2"); /* displays the string on lcd*/
+		lcd_gotoxy(0,2);
+		_delay_ms(1000);
+		lcd_puts("test program 3"); /* displays the string on lcd*/
+		lcd_gotoxy(0,3);
+		_delay_ms(1000);
+		lcd_puts("test program 4"); /* displays the string on lcd*/
+		_delay_ms(1000);
 
 	}
-/*
-	LCD_printStr("Circuits Today");
-	_delay_ms(5000);
-	LCD_cmd(0x01);
-	LCD_printStr("Print an Integer");
-	LCD_cmd(0xc0);
-	LCD_printInt(990);
-*/
 	return 0;
 }
+
